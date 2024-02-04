77bb02b84ad6c1e045f16763ad7d162b8df34ba3
Pdannyable <pdannyable@gmail.com>
2024-02-04 16:58:36 -0600

    updated directory name

---
+++ extra/account.py
@@ -0,0 +1,27 @@
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, n):
+        self._balance += n
+        print("The amount deposited is:", self._balance)
+
+    def withdraw(self, n):
+        self._balance -= n
+        print("The amount withdrawn is:", self._balance)
+
+
+def main():
+    account = Account()
+    print("Balance:", account.balance)
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance:", account.balance)
+
+
+if __name__ == "__main__":
+    main()
