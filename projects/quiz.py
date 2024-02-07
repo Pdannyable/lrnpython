436ae35b0292440560f4d63199a37c2a76c1b06f
Pdannyable <pdannyable@gmail.com>
2024-02-07 16:53:45 -0600

    Projects Directory added

---
+++ projects/quiz.py
@@ -0,0 +1,10 @@
+print("Welcome to the quiz game")
+
+
+playing = input("Would you like to play? ")
+if playing != "Yes":
+    quit()
+
+
+name = input("Please enter your name: ")
+print("Welcome to quiz game,", name)
